56c4b4984f75abb11de329cc3033cc48c89a94bc
RONALD-ALVAREZ-\humor <ronald.alvarez@uab.edu.bo>
2022-04-25 16:23:42 -0400

    maquetacion de interfaz de administracion de categorias y registro en la base de datos.

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/usuarios.controlador.php";
+require_once "controladores/categorias.controlador.php";
+
+require_once "modelos/categorias.modelo.php";
 require_once "modelos/usuarios.modelo.php";
 
 $plantilla = new ControladorPlantilla();
