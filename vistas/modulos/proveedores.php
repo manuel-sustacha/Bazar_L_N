9ecf4391c4082594c7c440bdc8fbb95adcf38d42
WINDOWS-O341UUP\Julio <julio.apaza2@uab.edu.bo>
2022-04-28 19:53:26 -0400

    Edicion de textos

--- vistas/modulos/proveedores.php
+++ vistas/modulos/proveedores.php
@@ -16,7 +16,7 @@
     
 
       <!-- Formulario Registro -->
-      <div class="modal fade " id="mdlProveedor" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="overflow-y: scroll;">
+      <!-- <div class="modal fade " id="mdlProveedor" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="overflow-y: scroll;">
           <div class="modal-dialog modal-sm">
               <div class="modal-content  text-navy">
                   <div class="modal-header">
@@ -52,7 +52,40 @@
                           <button type="button" class="btn btn-warning" aria-label="Close" data-dismiss="modal">Cerrar</button>
                           <button type="submit" id="btnguardarRol" class="btn" style="background:purple ; color:#ffffff">Guardar</button>
                       </div>
-                  </form>
+                  </form> -->
+                  <div class="modal" id="mdlProveedor" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Registrar</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <label class="m-0"></label>
+      <input type="text" class="form-control" id="rol" name="rol" placeholder="Ingresar Proveedor" required>
+      <label class="m-0"></label>
+      <input type="text" class="form-control" id="rol" name="rol" placeholder="Ingresar Telefono" required>
+      <label class="m-0"></label>
+      <input type="text" class="form-control" id="rol" name="rol" placeholder="Ingresar Direccion" required>
+      <label class="m-0"></label>
+      <input type="text" class="form-control" id="rol" name="rol" placeholder="Ingresar Telefono Repartidor" required>   
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Guardar Cambios</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Salir</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
 <!-- Formulario Registro -->
 
 
@@ -74,104 +107,56 @@
 
 <div>
 
-<table id="example" class="display" style="width:100%">
+<table class="table table-striped">
         <thead>
             <tr>
+                <th scope="col">id</th>
                 <th>Proveedor</th>
                 <th>Telefono</th>
-                <th>Office</th>
-                <th>Age</th>
                 <th>Direccion</th>
-                <th>Telefono</th>
+                <th>Telefono Repartidor</th>
             </tr>
         </thead>
         <tbody>
             <tr>
+                <th scope="row">1</th>
                 <td>Julio Cesar</td>
                 <td>2-1365840</td>
-                <td>Edinburgh</td>
-                <td>61</td>
                 <td>C/Martin Cardenas</td>
                 <td>71320800</td>
             </tr>
             <tr>
+                <th scope="row">2</th>
                 <td>Abram Maquera</td>
                 <td>2-2150036</td>
-                <td>Tokyo</td>
-                <td>63</td>
                 <td>C/Martin Cardenas</td>
                 <td>71170750</td>
             </tr>
             <tr>
+                <th scope="row">3</th>
                 <td>Manuel Fernando</td>
                 <td>2-3298520</td>
-                <td>San Francisco</td>
-                <td>66</td>
                 <td>C/Martin Cardenas</td>
                 <td>7186000</td>
             </tr>
             <tr>
+                <th scope="row">4</th>
                 <td>Cedric Kelly</td>
                 <td>2-36571024</td>
-                <td>Edinburgh</td>
-                <td>22</td>
                 <td>C/Martin Cardenas</td>
                 <td>71433060</td>
         </tbody>
         <tfoot>
             <tr>
+                <th scope="col">id</th>
                 <th>Proveedor</th>
                 <th>Telefono</th>
-                <th>Office</th>
-                <th>Age</th>
                 <th>Direccion</th>
                 <th>Telefono Repartidor</th>
             </tr>
         </tfoot>
     </table>
 
-
-<!-- <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">Proveedor</th>
-      <th scope="col">Telefono</th>
-      <th scope="col">Direccion</th>
-      <th scope="col">Telefono Proveedor</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Scarleth Fernandez Bauitista </td>
-      <td>2-2965241</td>
-      <td>C/Armando Rios Z/Equipetrol</td>
-      <td>68547021</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Scarleth Fernandez Bauitista </td>
-      <td>2-2965241</td>
-      <td>C/Armando Rios Z/Equipetrol</td>
-      <td>68547021</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Scarleth Fernandez Bauitista </td>
-      <td>2-2965241</td>
-      <td>C/Armando Rios Z/Equipetrol</td>
-      <td>68547021</td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>Scarleth Fernandez Bauitista </td>
-      <td>2-2965241</td>
-      <td>C/Armando Rios Z/Equipetrol</td>
-      <td>68547021</td>
-    </tr>
-</tbody>
-</table> -->
 </div>
     <!-- Default box -->
 
